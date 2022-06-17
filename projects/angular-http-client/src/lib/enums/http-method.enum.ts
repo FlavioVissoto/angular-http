f7c76a53f4f097c8498fa3ca06165d51bb0715d1
Flavio Vieira Vissoto <vissoto_flavio@hotmail.com>
2022-06-17 16:25:42 -0300

    Requisição HTTP

---
+++ projects/angular-http-client/src/lib/enums/http-method.enum.ts
@@ -0,0 +1,14 @@
+/**
+ * Protocolo HTTP utilizado nas requisições.
+ */
+export const enum HttpMethod {
+  Get = 'get',
+  Post = 'psot',
+  Put = 'put',
+  Head = 'head',
+  Delete = 'delete',
+  Patch = 'patch',
+  Options = 'options',
+  Connect = 'connect',
+  Trace = 'trace',
+}
---
+++ projects/angular-http-client/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of angular-http-client
+ */
+export * from './lib/services/http-request.service';
+export * from './lib/enums/http-method.enum';
+export * from './lib/interfaces/http-parameters.interface';
