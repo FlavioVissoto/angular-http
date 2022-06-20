ed5d746c9e8f70b02c36ea6bb9750e4279f012ed
FlavioVissoto <vissoto_flavio@hotmail.com>
2022-06-20 16:53:10 -0300

    Create card.module.ts

---
+++ projects/ui/src/lib/card/card.module.ts
@@ -0,0 +1,9 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+  declarations: [CardComponent],
+  imports: [CommonModule],
+  exports: [CardComponent],
+})
+export class UICardModule {}
