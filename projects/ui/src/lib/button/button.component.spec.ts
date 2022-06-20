193c6de6d39f0e0cdebd7552bd9b865e87a93a37
FlavioVissoto <vissoto_flavio@hotmail.com>
2022-06-20 16:49:01 -0300

    Update button.component.spec.ts

--- projects/ui/src/lib/button/button.component.spec.ts
+++ projects/ui/src/lib/button/button.component.spec.ts
@@ -1,23 +1,49 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
 
-import { ButtonComponent } from './button.component';
+import { By } from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { IonIUButtonComponent } from './iui-button.component';
 
-describe('ButtonComponent', () => {
-  let component: ButtonComponent;
-  let fixture: ComponentFixture<ButtonComponent>;
+describe('IonIUButtonComponent', () => {
+  let component: IonIUButtonComponent;
+  let fixture: ComponentFixture<IonIUButtonComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ButtonComponent ]
-    })
-    .compileComponents();
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [IonIUButtonComponent],
+      imports: [FontAwesomeModule],
+    }).compileComponents();
+  }));
 
-    fixture = TestBed.createComponent(ButtonComponent);
+  test('should create component', () => {
+    fixture = TestBed.createComponent(IonIUButtonComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    expect(component).toBeTruthy();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  test('should emit tab on click', () => {
+    fixture = TestBed.createComponent(IonIUButtonComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    const button = fixture.debugElement.query(By.css('button'));
+    component.clickedButton.subscribe((x: boolean) => {
+      expect(x).toEqual(true);
+    });
+
+    button.triggerEventHandler('click', null);
+  });
+
+  test('should emit tab on click with onloading', () => {
+    fixture = TestBed.createComponent(IonIUButtonComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    component.loading = true;
+    const button = fixture.debugElement.query(By.css('button'));
+    component.clickedButton.subscribe((x: boolean) => {
+      expect(x).toEqual(true);
+    });
+
+    button.triggerEventHandler('click', null);
   });
 });
