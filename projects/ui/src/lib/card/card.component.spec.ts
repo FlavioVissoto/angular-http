2bb233f0252d3ec36c3a643af976613b683e5a5e
FlavioVissoto <vissoto_flavio@hotmail.com>
2022-06-20 16:53:36 -0300

    Update card.component.spec.ts

--- projects/ui/src/lib/card/card.component.spec.ts
+++ projects/ui/src/lib/card/card.component.spec.ts
@@ -1,23 +1,44 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
 
-import { CardComponent } from './card.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { IonUICardComponent } from './iui-card.component';
 
-describe('CardComponent', () => {
-  let component: CardComponent;
-  let fixture: ComponentFixture<CardComponent>;
+describe('IonIUButtonComponent', () => {
+  let component: IonUICardComponent;
+  let fixture: ComponentFixture<IonUICardComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ CardComponent ]
-    })
-    .compileComponents();
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [IonUICardComponent],
+      imports: [FontAwesomeModule],
+    }).compileComponents();
+  }));
 
-    fixture = TestBed.createComponent(CardComponent);
+  test('should create component', () => {
+    fixture = TestBed.createComponent(IonUICardComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    expect(component).toBeTruthy();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  test('should emit clickCard on click', () => {
+    fixture = TestBed.createComponent(IonUICardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    component.clickCard();
+    component.clickedButton.subscribe((x: boolean) => {
+      expect(x).toEqual(true);
+    });
+  });
+
+  test('should emit clickCard on click with onloading', () => {
+    fixture = TestBed.createComponent(IonUICardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    component.loading = true;
+    component.clickCard();
+    component.clickedButton.subscribe((x: boolean) => {
+      expect(x).toEqual(false);
+    });
   });
 });
