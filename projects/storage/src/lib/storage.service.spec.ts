5a4809d9f7450f12afa70e6f6e15262e9ad802d4
FlavioVissoto <vissoto_flavio@hotmail.com>
2022-06-20 12:17:02 -0300

    storage.services.spec.ts

--- projects/storage/src/lib/storage.service.spec.ts
+++ projects/storage/src/lib/storage.service.spec.ts
@@ -1,16 +1,62 @@
+import { StorageServices } from './storage.services';
 import { TestBed } from '@angular/core/testing';
 
-import { StorageService } from './storage.service';
-
-describe('StorageService', () => {
-  let service: StorageService;
+describe('StorageServices', () => {
+  const mockValues = {
+    key: 'chave',
+    value: 'valor',
+  };
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(StorageService);
+    TestBed.configureTestingModule({
+      providers: [StorageServices],
+    }).compileComponents();
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  test('should create', () => {
+    expect(StorageServices).toBeTruthy();
+  });
+
+  test('should getLocal', () => {
+    localStorage.setItem(mockValues.key, JSON.stringify(mockValues));
+    expect(StorageServices.getLocal(mockValues.key)).toEqual(mockValues);
+  });
+
+  test('should setLocal', () => {
+    StorageServices.setLocal(mockValues.key, mockValues);
+    expect(StorageServices.getLocal(mockValues.key)).toEqual(mockValues);
+  });
+
+  test('should removeLocal', () => {
+    StorageServices.removeLocal(mockValues.key);
+    expect(StorageServices.getLocal(mockValues.key)).toBe(null);
+  });
+
+  test('should getSession', () => {
+    sessionStorage.setItem(mockValues.key, JSON.stringify(mockValues));
+    expect(StorageServices.getSession(mockValues.key)).toEqual(mockValues);
+  });
+
+  test('should setSession', () => {
+    StorageServices.setSession(mockValues.key, mockValues);
+    expect(StorageServices.getSession(mockValues.key)).toEqual(mockValues);
+  });
+
+  test('should removeSession', () => {
+    StorageServices.removeSession(mockValues.key);
+    expect(StorageServices.getSession(mockValues.key)).toBe(null);
+  });
+
+  test('should removeAll', () => {
+    StorageServices.removeAll(mockValues.key);
+    expect(StorageServices.getLocal(mockValues.key)).toBe(null);
+    expect(StorageServices.getSession(mockValues.key)).toBe(null);
+  });
+
+  test('should clearAll', () => {
+    StorageServices.clearAll();
+    expect(localStorage.length).toBe(0);
+    expect(sessionStorage.length).toBe(0);
   });
 });
+
