92e4d28fa9e26dc45da738d10e6fe5c8f79d1b3c
FlavioVissoto <vissoto_flavio@hotmail.com>
2022-06-20 16:49:22 -0300

    Update button.component.ts

--- projects/ui/src/lib/button/button.component.ts
+++ projects/ui/src/lib/button/button.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+import { faCircleNotch } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
-  selector: 'vat-button',
-  templateUrl: './button.component.html',
-  styleUrls: ['./button.component.css']
+  selector: 'iui-button',
+  templateUrl: './iui-button.component.html',
+  styleUrls: ['./../../assets/css/tailwind.scss'],
 })
-export class ButtonComponent implements OnInit {
+export class IonIUButtonComponent {
+  @Input() text: string;
+  @Input() disabled = false;
+  @Input() loading = false;
+  @Input() classCss: string;
+  @Input() classCssText: string;
+  @Input() type: 'button' | 'submit' | 'reset' = 'button';
 
-  constructor() { }
+  @Output() clickedButton = new EventEmitter<boolean>();
 
-  ngOnInit(): void {
+  faCircle = faCircleNotch;
+
+  clickButton(): void {
+    if (this.loading || this.disabled) {
+      return;
+    }
+    this.clickedButton.emit(true);
   }
-
 }
