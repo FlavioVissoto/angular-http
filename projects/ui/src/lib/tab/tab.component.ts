d748f4ee838e762dd9e17e9649802f384b0396a7
FlavioVissoto <vissoto_flavio@hotmail.com>
2022-06-20 16:54:46 -0300

    Update tab.component.ts

--- projects/ui/src/lib/tab/tab.component.ts
+++ projects/ui/src/lib/tab/tab.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+import { IonUITab } from './interfaces/iui-tab.interface';
 
 @Component({
-  selector: 'vat-tab',
-  templateUrl: './tab.component.html',
-  styleUrls: ['./tab.component.css']
+  selector: 'iui-tab',
+  templateUrl: './iui-tab.component.html',
 })
-export class TabComponent implements OnInit {
+export class IonUITabComponent {
+  @Input() Tabs: IonUITab[] = [];
 
-  constructor() { }
+  /**
+   * Evento disparado quando usuário selecionar um item.
+   */
+  @Output() clickedTab = new EventEmitter<IonUITab>();
 
-  ngOnInit(): void {
+  /**
+   *
+   * @param tab Item selecionado pelo usuário no template.
+   */
+  clickTab(tab: IonUITab): void {
+    this.setSelected(tab);
+    this.clickedTab.emit(tab);
   }
 
+  setSelected(tab: IonUITab): void {
+    for (let index = 0; index < this.Tabs.length; index++) {
+      this.Tabs[index].selected = this.Tabs[index].id === tab.id && this.Tabs[index].text === tab.text;
+    }
+  }
 }
