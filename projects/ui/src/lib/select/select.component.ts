8833591532cb7bbf5fc67cea6d561177bfc629c9
FlavioVissoto <vissoto_flavio@hotmail.com>
2022-07-08 10:19:11 -0300

    Update select.component.ts

--- projects/ui/src/lib/select/select.component.ts
+++ projects/ui/src/lib/select/select.component.ts
@@ -6,7 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./select.component.scss'],
 })
 export class SelectComponent implements OnInit {
+  @Input() card: Card;
+  @Input() title: string;
+  @Input() message: string;
+  @Input() messageFooter: string;
+  @Input() image: Image;
+
+  @Input() disabled = false;
+  @Input() loading = false;
+
+  @Output() clickedButton = new EventEmitter();
+
   ngOnInit(): void {
-    console.log('init');
+    if (!this.card) {
+      this.card = {
+        image: this.image,
+        title: this.title,
+        message: this.message,
+        messageFooter: this.messageFooter,
+      } as Card;
+    }
+  }
+
+  clickCard(): void {
+    if (this.loading || this.disabled) {
+      return;
+    }
+    this.clickedButton.emit(true);
   }
 }
