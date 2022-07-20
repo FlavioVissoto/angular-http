17061cbaf1ff75ec265cf1c2ba3ab92deaf760ff
Flavio Vieira Vissoto <vissoto_flavio@hotmail.com>
2022-07-20 17:16:22 -0300

    Instalação componente UI e configuração do Husky

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { NgModule } from '@angular/core';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { TooltipModule } from '@vissoto-angular-toolkit/ui';
 
 @NgModule({
   declarations: [SidebarComponent, NavbarComponent, NavbarComponent],
   exports: [SidebarComponent, NavbarComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, TooltipModule],
 })
 export class ComponentsModule {}
