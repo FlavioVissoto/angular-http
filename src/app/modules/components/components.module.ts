a163b891456d9cf6ee01e9a9bb7df9cb0b9304a5
Flavio Vieira Vissoto <vissoto_flavio@hotmail.com>
2022-08-16 17:26:15 -0300

    Tooltip icons

--- src/app/modules/components/components.module.ts
+++ src/app/modules/components/components.module.ts
@@ -5,11 +5,12 @@ import { NgModule } from '@angular/core';
 import { SanitizeHtmlPipe } from 'src/app/pipes/sanitize.pipe';
 import { SharingComponentsModule } from 'src/app/components/components.module';
 import { TooltipComponent } from './ui/tooltip/tooltip.component';
+import { TooltipIconsComponent } from './ui/tooltip-icons/tooltip-icons.component';
 import { TooltipModule } from '@vissoto-angular-toolkit/ui';
 import { UiComponent } from './ui/ui.component';
 
 @NgModule({
-  declarations: [ComponentsComponent, UiComponent, TooltipComponent],
+  declarations: [ComponentsComponent, UiComponent, TooltipComponent, TooltipIconsComponent],
   providers: [SanitizeHtmlPipe],
   imports: [CommonModule, ComponentsRoutingModule, SharingComponentsModule, TooltipModule],
 })
